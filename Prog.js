42b70ab2046ce46433cf0bd7c7c482097c2811cc
Rakesh <sairakesh217@gmail.com>
2020-01-27 17:03:21 -0500

    Commiting Algorithms

--- Prog.js
+++ Prog.js
@@ -1,4 +1,10 @@
-function maxChar(str){
-    word = str;
-    
+function isPrime(num){
+    for(i = 2; i < num; i++){
+        if(num % i == 0){
+            return "Not Prime";
+        }else{
+            return "Prime";
+        }
+    }
 }
+console.log(isPrime(5));
--- algorithms.js
+++ algorithms.js
@@ -36,4 +36,36 @@ function fizzBuzz(numb) {
     } return numb;
 }
 console.log(fizzBuzz(10));
-//--------------------------------------------------------
+//-------------------------------------------------------- 
+function chkAnag(word1, word2) {
+    if (word1.length == word2.length) {
+        split1 = word1.split(' ');
+        split2 = word2.split(' ');
+        for (i = 0; i < split1.length; i++) {
+            if (split1[i] == split2[i]) {
+            }
+        } return true;
+    } else {
+        return 'This is not a anagram.'
+    }
+}
+console.log(chkAnag('Hello World', 'Iaman Crawl'));
+//---------------------------------------------------------
+function arrChunk(arr, chunk) {
+    let tempArr1 = [];
+    let tempArr2 = [];
+    tempArr1 = arr.splice(0, chunk);
+    tempArr2 = arr;
+    return tempArr1 + "  " + tempArr2;
+}
+console.log(arrChunk([1, 2, 3, 4], 2));
+//---------------------------------------------------------
+function fibonacci(num) {
+    let temp = 0;
+    for (i = 0; i <= num; i++) {
+        temp += i;
+    }
+    return temp;
+}
+console.log(fibonacci(10));
+//---------------------------------------------------------
